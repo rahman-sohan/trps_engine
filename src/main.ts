f8c1c5afe9b8c84286e1cb44ab9e97632852976c
rahman-sohan <Sohanur@Scalebridger.com>
2025-05-23 23:48:40 +0600

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    });
+
     app.useGlobalPipes(new ValidationPipe());
-    
+
     await app.startAllMicroservices();
 
     await app.listen(process.env.PORT ?? 3000);
